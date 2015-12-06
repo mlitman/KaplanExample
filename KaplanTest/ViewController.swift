fe83a44d0b962fed0f1431c669c446a8663c582d
Mike Litman <mumbl1@mac.com>
2015-12-06 13:35:35 -0600

    initial commit

--- KaplanTest/ViewController.swift
+++ KaplanTest/ViewController.swift
@@ -8,13 +8,21 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController
+{
 
-    override func viewDidLoad() {
+    @IBOutlet weak var theLabel: UILabel!
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func changeTextButtonPressed(sender: AnyObject)
+    {
+        self.theLabel.text = "Kaplan"
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
